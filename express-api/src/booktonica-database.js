395bc0ff94c72122c4a1a137e6a92cb3077ad3ec
Bill D <355438+vegetabill@users.noreply.github.com>
2020-03-24 22:52:39 -0700

    skeleton API server

---
+++ express-api/src/booktonica-database.js
@@ -0,0 +1,37 @@
+const pgp = require('pg-promise')();
+
+/**
+ * An object that has methods matching useful database queries.
+ * Use `this.db` to access a connected pg-promise connection.
+ * Make sure to return the promise!
+ *
+ * For examples of other queries, see
+ * [pghttps://github.com/vitaly-t/pg-promise/wiki/Learn-by-Example
+ */
+class BooktonicaDatabase {
+  /**
+   * @param {String} name - name of database to connect to
+   */
+  constructor(name) {
+    const connectionString = `postgres://localhost:5432/${name}`;
+    console.log('Postgres DB => ', connectionString);
+    this.db = pgp(connectionString);
+  }
+
+  sanityCheck() {
+    console.log('\tTesting database connection...');
+    return this.getBooksCount().then(count =>
+      console.log(`\t✔️ Found ${count} books.`)
+    );
+  }
+
+  getBooksCount() {
+    return this.db.one('SELECT count(*) FROM books').then(r => r.count);
+  }
+
+  getAllBooks() {
+    return this.db.any('SELECT * FROM books');
+  }
+}
+
+module.exports = BooktonicaDatabase;
